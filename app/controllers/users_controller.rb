32920cb7989d61ef8c1f29d5baffca8661034d7f
Beth Becerra <BB8@Bethzaits-MBP.fritz.box>
2018-05-25 15:37:38 +0200

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    
   end
   
   def new
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to te Sample App!"
       redirect_to @user
     else
@@ -21,6 +23,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user) .permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
